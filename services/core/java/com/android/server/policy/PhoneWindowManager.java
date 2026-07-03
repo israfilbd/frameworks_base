ae2f61b9b896edbf91b22454a207ced863578c25
Cobra_Fast <c@ezl.re>
2026-07-03 15:53:51 +0200

    services: Add screen-on memory reclaim toggle
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/1059
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -873,6 +873,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private ThreeFingersSwipeListener mThreeFingersSwipe;
     private boolean mThreeFingersSwipeHasAction;
 
+    private boolean mScreenOnReclaim;
+
     private PocketManager mPocketManager;
     private PocketLock mPocketLock;
     private boolean mPocketLockShowing;
@@ -1177,6 +1179,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.KEY_SHAKE_GESTURE_ACTION), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREEN_ON_MEMORY_RECLAIM), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -3745,6 +3750,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mGlobalActionsOnLockEnable = Settings.System.getIntForUser(resolver,
                     Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1,
                     UserHandle.USER_CURRENT) != 0;
+
+            mScreenOnReclaim = Settings.System.getIntForUser(resolver,
+                    Settings.System.SCREEN_ON_MEMORY_RECLAIM, 1, UserHandle.USER_CURRENT) == 1;
 
             kidsModeEnabled = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.NAV_BAR_KIDS_MODE, 0, UserHandle.USER_CURRENT) == 1;
@@ -6588,8 +6596,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         EventLogTags.writeScreenToggled(1);
 
-        mHandler.removeCallbacks(mMemoryOpt);
-        mHandler.postDelayed(mMemoryOpt, 1250 /* allowance time */);
+        if (mScreenOnReclaim) {
+            mHandler.removeCallbacks(mMemoryOpt);
+            mHandler.postDelayed(mMemoryOpt, 1250 /* allowance time */);
+        }
 
         // remove pending system server gc for frequent screen state changes
         mHandler.removeCallbacks(mSystemServerGcOpt);
